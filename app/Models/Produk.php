5519aaa49b94794a76898c62b6a247d64aee2146
FiqSky <fiqihfm05@gmail.com>
2021-08-11 00:19:42 +0700

    initial commit

---
+++ app/Models/Produk.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama','harga', 'warna', 'kondisi', 'deskripsi'
+    ];
+}
